785e654a4cd5b45e49fc6e57084bb4ad156f6f10
Charles <info@servesolutions.io>
2023-03-27 21:48:54 +0100

    added module in main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
-module "network" {
-    source = "./network"
-  
+module "VPC" {
+  source                              = "./network/VPC"
+  region                              = var.region
+  vpc_cidr                            = var.vpc_cidr
+  enable_dns_support                  = var.enable_dns_support
+  enable_dns_hostnames                = var.enable_dns_hostnames
+  enable_classiclink                  = var.enable_classiclink
+  preferred_number_of_public_subnets  = var.preferred_number_of_public_subnets
+  preferred_number_of_private_subnets = var.preferred_number_of_private_subnets
+  private_subnets                     = [for i in range(1, 8, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
+  public_subnets                      = [for i in range(2, 5, 2) : cidrsubnet(var.vpc_cidr, 8, i)]
 }
 
+module "security" {
+  source = "./network/security"
+  vpc_id = module.VPC.vpc_id
+}
