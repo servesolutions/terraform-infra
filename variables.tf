c05f430b567ff930e88df6287800e0e4076345ac
Charles <info@servesolutions.io>
2023-03-27 22:01:24 +0100

    updated variable.tf file

--- variables.tf
+++ variables.tf
@@ -1,3 +1,33 @@
-variable "aws_region" {
+variable "region" {
   default = "eu-west-2"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "enable_dns_support" {
+  default = "true"
+}
+
+variable "enable_dns_hostnames" {
+  default = "true"
+}
+
+variable "enable_classiclink" {
+  default = "false"
+}
+
+variable "enable_classiclink_dns_support" {
+  default = "false"
+}
+
+variable "preferred_number_of_public_subnets" {
+  type        = number
+  description = "Number of public subnets"
+}
+
+variable "preferred_number_of_private_subnets" {
+  type        = number
+  description = "Number of private subnets"
 }
