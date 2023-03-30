bd91c0bd70720ce1e6839840982e7b0941a14a92
John Nzeako <johnnzeako@Johns-MBP.broadband>
2023-03-30 23:22:56 +0100

    adding backend9

--- modules/vpc/roles.tf
+++ modules/vpc/roles.tf
@@ -48,7 +48,7 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
 }
 
 resource "aws_iam_instance_profile" "ip" {
-  name = "aws_instance_profile_test"
+  name = "aws_instance_profile_test_2"
   role = aws_iam_role.ec2_instance_role.name
 }
 
